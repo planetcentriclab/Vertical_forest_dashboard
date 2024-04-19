ec84f74333579d5dbde1587458e319b367e24081
grafftkdt <akarapong.mars@mail.kmutt.ac.th>
2024-04-19 18:41:22 +0700

    first commit

---
+++ src/components/Content.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Grid, GridItem, Avatar, AvatarBadge, AvatarGroup,Stat ,StatLabel, StatNumber, StatHelpText, StatArrow, StatGroup, Card, CardBody} from '@chakra-ui/react'
+
+function Content() {
+  return (
+    <main>
+        <Grid templateColumns='repeat(4, 1fr)' gap={5}>
+            <Card>
+            <CardBody>
+                <Stat>
+                    <StatLabel fontSize= 'lg'color= 'gray.600'>Temperature</StatLabel>
+                    <StatNumber fontSize= '5xl'>33.21°C</StatNumber>
+                    <StatHelpText fontSize= 'md'>
+                    <StatArrow type='decrease' />
+                    1.23°C
+                    Since last week.
+                    </StatHelpText>
+                </Stat>
+            </CardBody>
+            </Card>
+            <Card>
+            <CardBody>
+                <Stat>
+                    <StatLabel fontSize= 'lg'color= 'gray.600'>Humidity</StatLabel>
+                    <StatNumber fontSize= '5xl'>60.55%</StatNumber>
+                    <StatHelpText fontSize= 'md'>
+                    <StatArrow type='increase' />
+                    23.36%
+                    Since last week.
+                    </StatHelpText>
+                </Stat>
+            </CardBody>
+            </Card>
+            <Card>
+            <CardBody>
+                <Stat>
+                    <StatLabel fontSize= 'lg'color= 'gray.600'>Soil Temperature</StatLabel>
+                    <StatNumber fontSize= '5xl'>28.38°C</StatNumber>
+                    <StatHelpText fontSize= 'md'>
+                    <StatArrow type='increase' />
+                    3.84°C
+                    Since last week.
+                    </StatHelpText>
+                </Stat>
+            </CardBody>
+            </Card>
+            <Card>
+            <CardBody>
+                <Stat>
+                    <StatLabel fontSize= 'lg'color= 'gray.600'>Soil Humidity</StatLabel>
+                    <StatNumber fontSize= '5xl'>76.57%</StatNumber>
+                    <StatHelpText fontSize= 'md'>
+                    <StatArrow type='increase' />
+                    1.23%
+                    Since last week.
+                    </StatHelpText>
+                </Stat>
+            </CardBody>
+            </Card>
+        </Grid>
+    </main>
+    
+  )
+}
+
+export default Content
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function Footer() {
+  return (
+    <>
+      <footer>
+        <p>Coryright 2024</p>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
